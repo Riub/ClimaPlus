c831919e8794326099e5219dc4553c1d1329241e
Jeison <dracon_019@hotmail.com>
2025-05-24 23:29:32 -0500

    Front add

---
+++ climaplus-frontend/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>ClimaPlus</h2>
+      <h2>Bienvenidos a tu app del Clima</h2>
+    </div>
+  );
+}
+
+export default App;
