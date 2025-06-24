33a236694671652784caa372106a186c03905825
Jeison <dracon_019@hotmail.com>
2025-06-23 19:31:29 -0500

    cambio para navbar en favorites

--- climaplus-frontend/src/components/FavoritesView.js
+++ climaplus-frontend/src/components/FavoritesView.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import NavBar from './NavBar';
+import NavBar from './Navbar';
 
 const FavoritesView = ({ user, onLogout, setView }) => {
   const [favorites, setFavorites] = useState([]);
